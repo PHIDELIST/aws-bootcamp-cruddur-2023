097165fb4af5f56e0926ac44560b4e5d62fbefc6
PHIDELIST <phidelisoluoch@gmail.com>
2023-03-16 19:42:46 +0000

    updated lambda layer python version

--- aws-psycopg2/get_layer_packages.sh
+++ aws-psycopg2/get_layer_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PKG_DIR="python"
 rm -rf ${PKG_DIR} && mkdir -p ${PKG_DIR}
-docker run --rm -v $(pwd):/foo -w /foo lambci/lambda:build-python3.6 \
+docker run --rm -v $(pwd):/foo -w /foo lambci/lambda:build-python3.9 \
 	pip install -r requirements.txt --no-deps -t ${PKG_DIR}
 
