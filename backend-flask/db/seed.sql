62dadd6c3ee29f20b3e5f46941aff2ca10f34da6
PHIDELIST <phidelisoluoch@gmail.com>
2023-03-17 21:38:53 +0000

    All errors solved. all week 4 work done

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,13 +1,12 @@
 -- this file was manually created
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-  ('PHIDELIST OLUOCH', 'phidelisomuya' ,'MOCK'),
-  ('Andrew Bayko', 'bayko' ,'MOCK');
-
+  ('phidel', 'phidel' , 'phidelisoluoch@gmail.com' , 'e0b69ea1-57e2-42ad-8025-dd372666785d');
+  
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'phidelisomuya' LIMIT 1),
-    'This was imported as seed data!',
+    (SELECT uuid from public.users WHERE users.handle = 'phidel' LIMIT 1),
+    'This is my first crudd!',
     current_timestamp + interval '10 day'
   )
