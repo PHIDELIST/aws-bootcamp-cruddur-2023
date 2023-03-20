8a97e11be0d2b3627809d47ef1016aec73c3a51e
PHIDELIST <phidelisoluoch@gmail.com>
2023-03-20 17:28:28 +0000

    Added utility scripts for dynamodb and added new user to seed.sql

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,7 +1,8 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-  ('phidel', 'phidel' , 'phidelisoluoch@gmail.com' , 'e0b69ea1-57e2-42ad-8025-dd372666785d');
+  ('phidel', 'phidel' , 'phidelisoluoch@gmail.com' , 'e0b69ea1-57e2-42ad-8025-dd372666785d'),
+  ('Sama', 'Sama' , 'oluoch.phidelist@students.kyu.ac.ke' , '1e5dc2a1-3c34-42e9-8ed4-b3b74b1e9b37');
   
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
