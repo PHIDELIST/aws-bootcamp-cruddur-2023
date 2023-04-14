beb14b9850e6f6cae0b8027818ebb1bdf333b331
PHIDELIST <phidelisoluoch@gmail.com>
2023-04-14 10:35:57 +0000

    created lamda and s3 stack

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -1,16 +1,58 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { config } from 'process';
+import * as dotenv from 'dotenv';
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+//load env variables
+
+dotenv.config();
 
 export class ThumbingServerlessCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+  const bucketName: string = process.env.THUMBING_BUCKET_NAME as string;
+  const functionPath: string = process.env.THUMBING_FUNCTION_PATH as string;
+  const folderInput: string = process.env.THUMBING_S3_FOLDER_INPUT as string;
+  const folderOutput: string = process.env.THUMBING_S3_FOLDER_OUTPUT as string;
+  
+  const bucket = this.createBucket(bucketName);
+  const lambda = this.createLambda(functionPath,
+    bucketName, 
+    folderOutput, 
+    folderInput);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ThumbingServerlessCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
+//creating bucket
+  createBucket(bucketName: string): s3.IBucket {
+    const bucket = new s3.Bucket(this, 'ThumbingBucket', {
+      bucketName: bucketName,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+    return bucket;
+  }
+  //creating lambda 
+  createLambda(functionPath: string, folderInput: string, folderOutput: string): lambda.IFunction {
+    const lambdaFunction = new lambda.Function(this, 'ThumbLambda',{
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(functionPath),
+      environment:{
+        DEST_BUCKET_NAME: bucketName,
+        FOLDER_INPUT: folderInput,
+        FOLDER_OUTPUT: folderOutput,
+        PROCESS_WIDTH: '512',
+        PROCESS_HEIGHT: '512'
+
+      }
+  });
+  return lambdaFunction;
+
+  }
+  //end creating lambda
+
 }
