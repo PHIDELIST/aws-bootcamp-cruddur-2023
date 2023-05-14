7f2797715cd9c8234a78ed9c614bda9237f0b1d8
PHIDELIST <phidelisoluoch@gmail.com>
2023-05-14 15:49:31 +0000

    corrected profile avatar fetch exptension from jpg to jpeg

--- frontend-react-js/src/components/ProfileAvatar.js
+++ frontend-react-js/src/components/ProfileAvatar.js
@@ -1,8 +1,8 @@
 import './ProfileAvatar.css';
 
 export default function ProfileAvatar({ id }) {
-  //const backgroundImage = `url("https://assets.cruddur.com/avatars/${props.id}.jpg")`;
-  const backgroundImage = id != null ? `url("https://assets.globalphidelist.tech/avatars/${id}.jpg")` :"none";
+  //const backgroundImage = `url("https://assets.cruddur.com/avatars/${props.id}.jpeg")`;
+  const backgroundImage = id != null ? `url("https://assets.globalphidelist.tech/avatars/${id}.jpeg")` :"none";
   const styles = {
     backgroundImage,
     backgroundSize: 'cover',
